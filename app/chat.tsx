217ec73571398b44f5f41cc98ce5cf883c6b6ab9
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-09-22 14:43:43 +0200

    Update chat.tsx

--- app/chat.tsx
+++ app/chat.tsx
@@ -5,13 +5,13 @@ import { useTranslation } from 'next-i18next';
 import '../lib/i18n';
 import { useSession } from "next-auth/react"; 
 import { useUI } from './context/UIContext';
-import { Capacitor } from '@capacitor/core';
 
 interface ModalProps {
   onClose: () => void;
+  game: number;
 }
 
-const Modal: React.FC<ModalProps> = ({ onClose }) => {
+const Modal: React.FC<ModalProps> = ({ onClose, game }) => {
   const { setIsModalOpen } = useUI();
   const { t } = useTranslation();
   const { data: session } = useSession();
@@ -19,46 +19,46 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
   // 👉 useState für isApp
-  const [isApp, setIsApp] = useState(false);
+  const [message, setMessage] = useState("");
 
   // Modal öffnen + Escape-Handler
   useEffect(() => {
-    const modal = modalRef.current;
     setIsModalOpen(true);
-
-    if (modal) modal.focus();
-    document.body.style.overflow = 'hidden';
-
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        onClose();
-      }
-    };
-    document.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-      setIsModalOpen(false);
-      document.body.style.overflow = 'auto';
-    };
   }, [onClose, setIsModalOpen]);
 
-  // Check ob native App
-  useEffect(() => {
-    setIsApp(Capacitor.isNativePlatform());
-  }, []);
+  
+
+  const handleSend = async () => {
+    
+    const res = await fetch("/api/chat/send", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        message: message,
+      }),
+    });
+
+    if (res.ok) {
+      
+      console.log("Chat send");
+      onClose();
+    } else {
+      console.log("Error on Chat");
+    }
+  
+  };
 
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black/50 backdrop-blur-sm z-50">
+    <div className="absolute inset-0 flex justify-center items-center bg-black/50 backdrop-blur-sm z-50">
       <div
         ref={modalRef}
         tabIndex={-1}
-        className="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-200 p-8 rounded-lg shadow-lg w-full max-w-3xl max-h-[80vh] overflow-hidden flex flex-col"
+        className="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-200 p-8 rounded-lg shadow-lg w-full max-w-3xl max-h-[80vh] flex flex-col"
       >
         {/* Modal Header */}
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-2xl font-semibold text-pink-600 dark:text-pink-400">
-            {t("Chat")}
+            Live Chat
           </h2>
           <button
             onClick={onClose}
@@ -67,21 +67,42 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
             X
           </button>
         </div>
-
+        <div className="min-h-[24v] min-w-full rounded-lg shadown-lg mb-2 border border-gray-300 dark:border-gray-600">
+        
+        </div>
         {/* Modal Content */}
         <div className="overflow-y-auto max-h-[70vh] space-y-4">
           {session ? (
-            <p>
-              {t("Willkommen")}, <strong>{session.user?.uname}</strong> 🎉
-            </p>
-          ) : (
-            <p>{t("Keine Session gefunden")}</p>
-          )}
-
-          <p className="text-sm text-gray-500">
-            {isApp ? t("Du nutzt die App 📱") : t("Du nutzt die Web-Version 💻")}
-          </p>
+        <div> 
+          <div className="inline-flex">
+           <textarea
+          className="w-full bottom-2 m-2 p-3 bg-white border border-gray-300 rounded-lg shadow-lg dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-pink-500"
+          placeholder={t("probDes")}
+          value={reportData}
+          onChange={(e) => {
+            if (e.target.value.length <= 100) {
+              setReportData(e.target.value);
+            }
+          }}
+          disabled={false}
+        />
+     
+            <button
+              onMouseDown={(e) => e.preventDefault()}
+              onClick={handleSend}
+              className="px-4 py-2 bg-pink-500 text-white rounded-lg hover:bg-pink-600 transition mt-4"
+            >
+              ➡️
+            </button>
+      </div>
+            <div className={`${reportData.length === 200 ? 'text-red-600' : ''}`}>
+              {reportData.length} / 100
+            </div>
+      
         </div>
+          ) : null}
+        </div>
+        
       </div>
     </div>
   );
