8ecb0a3c72e6f1e1f6f2fad86353dd7777111e6e
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-09-22 09:04:28 +0200

    Update gameDetails.tsx

--- app/gameDetails.tsx
+++ app/gameDetails.tsx
@@ -479,7 +479,7 @@ const formatTime = (ms: number) => {
                             />
                         </div>
                     )}
-                    <div>
+                    <div className="items-center justify-center">
                         <button className="text-sm text-blue-500 hover:underline mt-4" onClick={handleReportOpen}>
                             Report Problem
                         </button>
