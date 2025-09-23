6946dc9b6e568ac83ce5cc47ae637894e14a8032
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-09-23 11:18:17 +0200

    Update useOngoingNotification.ts

--- capacitor/useOngoingNotification.ts
+++ capacitor/useOngoingNotification.ts
@@ -65,7 +65,7 @@ export function useOngoingNotification() {
     const initNotification = async () => {
       try {
         await startOngoingNotification(
-          `Deine Punkte: ? \nVerbleibend: ${formatTime(
+          `Deine Punkte: ... \nVerbleibend: ${formatTime(
             endingRef.current.getTime() - Date.now()
           )}`
         );
