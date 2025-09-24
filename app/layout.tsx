ede290e0a21fefe5c19da19d58d45dc6b318cf53
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-09-24 17:41:34 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="de" class="">
+    <html lang="de">
         
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
