a4c97a7dcc6674a69a1dcdebb355644fae5e18cd
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-09-24 11:08:52 +0200

    Update page.tsx

--- app/scoreboard/page.tsx
+++ app/scoreboard/page.tsx
@@ -4,7 +4,6 @@ import { Capacitor } from "@capacitor/core";
 import React, { useEffect, useState } from "react";
 import Carousel from "../common/carousel";
 import { useTranslation } from 'next-i18next';
-import '../../lib/i18n'
 
 const { t, i18n } = useTranslation();  // Hook innerhalb der Komponente verwenden
 
