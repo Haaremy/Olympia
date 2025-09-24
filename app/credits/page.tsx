76d1f376cf346968f6eb74ced4d49a6b519ef0fd
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-09-24 21:39:44 +0200

    Update page.tsx

--- app/credits/page.tsx
+++ app/credits/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
   const { i18n } = useTranslation();
   const [platform, setPlatform] = useState("");
     const [isApp, setIsApp] = useState(false);
-  const [theme, setTheme] = useState<string | null>(null);
+  const [theme, setTheme] = useState<string>("auto");
 
   // Beim ersten Render den gespeicherten Wert aus localStorage holen
   useEffect(() => {
