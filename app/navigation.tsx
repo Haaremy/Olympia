3e78d5f661c2db6423d2949f88b7b7d741d86f7b
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-09-30 22:53:51 +0200

    Update navigation.tsx

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -408,7 +408,7 @@ if (lastChecked) {
                     </p>
                   </button>
                    )
-                  )
+                  
                 }
               <Link
                       href="/scoreboard"
