280007afd62b8082c98d91e366b7d69e49ea2d66
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-10-10 08:39:33 +0200

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -7,7 +7,8 @@ export default function Footer() {
   const { t } = useTranslation();  // Hook innerhalb der Komponente verwenden
 
   return (
-    <footer className=`bottom-0 left-0 w-full border-t shadow-md z-50
+    <footer className=`
+    bottom-0 left-0 w-full border-t shadow-md z-50
     bg-white border-gray-300  
     dark:bg-gray-800 dark:border-gray-700
     truedark:bg-black truedark:border-white
