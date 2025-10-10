9baeecd7563f4abf66a6a791bdc378d11af8bb1f
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-10-10 08:26:47 +0200

    Update main.tsx

--- cooperateDesign/main.tsx
+++ cooperateDesign/main.tsx
@@ -4,8 +4,12 @@ import clsx from 'clsx';
 
 export type MainProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-const baseStyles =
-  'bg-pink-100 dark:bg-gray-900 truedark:bg-black text-gray-900 dark:text-white truedark:text-white w-full min-h-screen flex flex-col items-center justify-start p-4 sm:p-8 pt-20 transition-all duration-300';
+const baseStyles = `
+w-full min-h-screen flex flex-col items-center justify-start p-4 sm:p-8 pt-20 transition-all duration-300
+bg-pink-100 text-gray-900
+dark:bg-gray-900 dark:text-white
+truedark:bg-black truedark:text-white 
+`;
 
 
 const Main = React.forwardRef<HTMLAnchorElement, MainProps>(
