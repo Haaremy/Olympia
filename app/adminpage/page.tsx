52207e16b024833491002d3922b4b0e8f79a62c2
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-11-28 18:07:05 +0100

    Add contact field to admin page

--- app/adminpage/page.tsx
+++ app/adminpage/page.tsx
@@ -14,6 +14,7 @@ type SearchedTeam = {
   name: string;
   players: string[];
   cheatPoints: number;
+  contact: string;
   games: {
     id: number;
     title: string;
@@ -361,6 +362,14 @@ const getOffsetISO = (dtLocal: string): string => {
                 type="text"
                 ref={nameTRef}
                 defaultValue={searchedTeam.name}
+                
+              />
+              <div className="mb-4">
+              <label className="block font-semibold mb-1">Kontakt:</label>
+              <TextInput
+                type="text"
+                ref={nameTRef}
+                defaultValue={searchedTeam.contact}
                 
               />
 
