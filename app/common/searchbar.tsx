d63ca20d17da82e92959cd31a669554356d2c9dc
Haaremy <48878177+Haaremy@users.noreply.github.com>
2025-12-02 06:58:33 +0100

    Create searchbar.tsx

---
+++ app/common/searchbar.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { TextInput } from "@cooperateDesign";
+import clsx from "clsx";
+import { useKeyboardOffset } from "@/common/useKeyboardOffset";
+
+export default function SearchBar({
+  searchQuery,
+  setSearchQuery,
+  isModalOpen,
+}: {
+  searchQuery: string;
+  setSearchQuery: (v: string) => void;
+  isModalOpen: boolean;
+}) {
+  const offset = useKeyboardOffset();
+  const keyboardVisible = offset > 0;
+
+  return (
+    <div
+      className={clsx(
+        isModalOpen ? "hidden" : "block",
+        "fixed left-1/2 -translate-x-1/2 z-[9999] w-[85%] sm:w-[50%] lg:w-[25%] max-w-md transition-all duration-150"
+      )}
+      style={{
+        bottom: keyboardVisible ? 0 : 20,
+        transform: keyboardVisible
+          ? `translateY(-${offset}px)`
+          : "translateY(0)",
+      }}
+    >
+      <TextInput
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+
+          setTimeout(() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }, 10);
+        }}
+        className="px-6 py-3 shadow-xl rounded-xl bg-white dark:bg-gray-700"
+      />
+    </div>
+  );
+}
